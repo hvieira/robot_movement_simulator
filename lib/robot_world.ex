04993f9bab2ab4aff480c5db36a885acfa5fae82
hvieira <hugo.r.f.s.vieira@gmail.com>
2022-04-02 16:58:30 +0100

    Change the world to only have a single robot. This way its easier to update the robot to its final state

--- lib/robot_world.ex
+++ lib/robot_world.ex
@@ -1,10 +1,10 @@
 defmodule RobotWorld do
   @moduledoc false
 
-  @type t :: %RobotWorld{width: integer, height: integer, robots: [Robot.t()]}
+  @type t :: %RobotWorld{width: integer, height: integer, robot: Robot.t() | nil}
 
   @enforce_keys [:width, :height]
-  defstruct [:width, :height, robots: []]
+  defstruct [:width, :height, robot: nil]
 
   @spec create(integer, integer) :: RobotWorld.t()
   def create(w, h), do: %RobotWorld{width: w, height: h}
@@ -16,10 +16,12 @@ defmodule RobotWorld do
   @spec place_robot(RobotWorld.t(), Robot.t()) :: RobotWorld.t()
   def place_robot(world, %Robot{x: x, y: y} = robot)
       when x < 0 or x >= world.width or (y < 0 or y >= world.height) do
-    %{world | robots: Enum.concat(world.robots, [%{robot | lost: true}])}
+    %{world | robot: %{robot | lost: true}}
   end
 
   def place_robot(world, robot) do
-    %{world | robots: Enum.concat(world.robots, [robot])}
+    %{world | robot: robot}
   end
+
+
 end
--- test/robot_world_test.exs
+++ test/robot_world_test.exs
@@ -2,14 +2,14 @@ defmodule RobotWorldTest do
   use ExUnit.Case
   doctest RobotWorld
 
-  describe "Place robots" do
+  describe "Place robot" do
     test "places a robot within world in its starting state" do
       assert RobotWorld.create(1, 1)
              |> RobotWorld.place_robot(%Robot{x: 0, y: 0, orientation: 'N'}) ==
                %RobotWorld{
                  width: 1,
                  height: 1,
-                 robots: [%Robot{x: 0, y: 0, orientation: 'N', lost: false}]
+                 robot: %Robot{x: 0, y: 0, orientation: 'N', lost: false}
                }
 
       assert RobotWorld.create(10, 10)
@@ -17,7 +17,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 10,
-                 robots: [%Robot{x: 7, y: 5, orientation: 'S', lost: false}]
+                 robot: %Robot{x: 7, y: 5, orientation: 'S', lost: false}
                }
 
       assert RobotWorld.create(7, 10)
@@ -25,7 +25,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 7,
                  height: 10,
-                 robots: [%Robot{x: 6, y: 0, orientation: 'E', lost: false}]
+                 robot: %Robot{x: 6, y: 0, orientation: 'E', lost: false}
                }
 
       assert RobotWorld.create(10, 7)
@@ -33,11 +33,11 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 7,
-                 robots: [%Robot{x: 0, y: 6, orientation: 'W', lost: false}]
+                 robot: %Robot{x: 0, y: 6, orientation: 'W', lost: false}
                }
     end
 
-    test "robots placed outside the world are deemed lost" do
+    test "robot placed outside the world are deemed lost" do
       world = RobotWorld.create(10, 10)
 
       assert world
@@ -45,7 +45,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 10,
-                 robots: [%Robot{x: -1, y: 0, orientation: 'N', lost: true}]
+                 robot: %Robot{x: -1, y: 0, orientation: 'N', lost: true}
                }
 
       assert world
@@ -53,7 +53,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 10,
-                 robots: [%Robot{x: 0, y: -1, orientation: 'N', lost: true}]
+                 robot: %Robot{x: 0, y: -1, orientation: 'N', lost: true}
                }
 
       assert world
@@ -61,7 +61,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 10,
-                 robots: [%Robot{x: 500, y: 0, orientation: 'N', lost: true}]
+                 robot: %Robot{x: 500, y: 0, orientation: 'N', lost: true}
                }
 
       assert world
@@ -69,7 +69,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 10,
-                 robots: [%Robot{x: 0, y: 100, orientation: 'N', lost: true}]
+                 robot: %Robot{x: 0, y: 100, orientation: 'N', lost: true}
                }
 
       assert world
@@ -77,7 +77,7 @@ defmodule RobotWorldTest do
                %RobotWorld{
                  width: 10,
                  height: 10,
-                 robots: [%Robot{x: 10, y: 10, orientation: 'N', lost: true}]
+                 robot: %Robot{x: 10, y: 10, orientation: 'N', lost: true}
                }
     end
   end
